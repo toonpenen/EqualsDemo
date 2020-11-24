b9a1bc90d9cfe0695f59dc8d1d0a074eb79a5ffb
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-24 11:33:13 +0100

    Add project files.

---
+++ EqualsDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 15;
+            int y = 15;
+
+            Console.WriteLine(x==y);
+            Console.WriteLine(x.Equals(y));
+        }
+    }
+}
